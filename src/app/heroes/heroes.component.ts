99337b3e92d02cad2cdd5a408ce93fadb91bb169
Noureddine F <nouredine.f@gmail.com>
2024-01-06 10:47:18 +0000

    Ajout service sans obervable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +9,25 @@ import { Hero } from '../hero';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Dinz'
-  };
+  constructor(private heroService: HeroService) { }
+
+  heroesList: Hero[] = []
+  selectedHero?: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero) {
+    if (this.selectedHero == hero) {
+      this.selectedHero = undefined;
+    } else {
+      this.selectedHero = hero
+    }
+  }
+
+  getHeroes(): void {
+    this.heroesList = this.heroService.getHeroes();
+  }
+
 }
