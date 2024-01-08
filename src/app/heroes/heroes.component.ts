86cacd4d2444f418bb4824247f86a182b9b2d24b
Noureddine F <nouredine.f@gmail.com>
2024-01-08 23:58:41 +0000

    Fin étape 4 tuto (msgService)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,7 @@ import { HeroService } from '../hero.service';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   heroesList: Hero[] = []
   selectedHero?: Hero;
@@ -17,17 +18,18 @@ export class HeroesComponent {
   ngOnInit(): void {
     this.getHeroes();
   }
-  
+
   onSelect(hero: Hero) {
     if (this.selectedHero == hero) {
       this.selectedHero = undefined;
     } else {
       this.selectedHero = hero
+      this.messageService.add(hero.name)
     }
   }
 
   getHeroes(): void {
-    this.heroesList = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(list => this.heroesList = list);
   }
 
 }
